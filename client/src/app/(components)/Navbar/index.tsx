5fa9b0dc0be1697c34cd6334968d6a9ed172aab9
Brian Lin <brian@differential.com>
2024-07-29 15:30:46 -0400

    add initial navbar

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+// when using an event handler (i.e onClick), need use client or else it will error
+
+import { Menu } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center w-full mb-7">
+      {/* Left side */}
+      <div className="flex justify-between items-center gap-5">
+        <button
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
