ce836bbaea7bdc0ba3acc9c8db96dbdb81da04d8
Brian Lin <brian@differential.com>
2024-08-08 17:07:00 -0400

    add dashboard route

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 // Route Imports
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 // Configs
 dotenv.config();
@@ -18,9 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
-app.get("/hello", (req, res) => {
-  res.send("hello world");
-});
+app.use("/dashboard", dashboardRoutes); //http://localhost:8000/dashboard
 //Server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
