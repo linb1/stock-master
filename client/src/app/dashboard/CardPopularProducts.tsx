6a8e8b590d2bc1ec1b29fe9ba25d8966ac1bf527
Brian Lin <brian@differential.com>
2024-08-09 15:13:12 -0400

    add initial product items

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -1,4 +1,5 @@
 import { useGetDashboardMetricsQuery } from "@/state/api";
+import { ShoppingBag } from "lucide-react";
 import React from "react";
 
 const CardPopularProducts = () => {
@@ -12,6 +13,38 @@ const CardPopularProducts = () => {
           <h3 className="text-lg font-semibold px-7 pt-5 pb-2">
             Popular Products
           </h3>
+          <hr />
+          <div className="overflow-auto h-full">
+            {dashboardMetrics?.popularProducts.map((product) => (
+              <div
+                key={product.productId}
+                className="flex item-center justify-between gap-3 px-5 py-7 border-b"
+              >
+                <div className="flex items-center gap-3">
+                  <div>img</div>
+                  <div className="flex flex-col justify-between gap-1">
+                    <div className="font-bold text-gray-700">
+                      {product.name}
+                    </div>
+                    <div className="flex text-sm items-center">
+                      <span className="font-bold text-blue-500 text xs">
+                        ${product.price}
+                      </span>
+                      <span className="mx-2">|</span>
+                      <div>Rating</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-xs flex items-center">
+                  <button className="p-2 rounded-full bg-blue-100 text-blue-600 mr-2">
+                    <ShoppingBag className="w-4 h-4" />
+                  </button>
+                  {Math.round(product.stockQuantity / 1000)}k Sold
+                </div>
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
