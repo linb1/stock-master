ee0fc4f19f7fc3ac4b7d4d888ca8d3a7b8eb014b
Brian Lin <brian@differential.com>
2024-08-12 14:36:46 -0400

    add total value sum, body and header

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -4,7 +4,11 @@ import React, { useState } from "react";
 const CardSalesSummary = () => {
   const { data, isLoading, isError } = useGetDashboardMetricsQuery();
   const saleData = data?.salesSummary || [];
+
   const [timeframe, setTimeframe] = useState("weekly");
+
+  const totalValueSum =
+    saleData.reduce((acc, curr) => acc + curr.totalValue, 0) || 0;
 
   if (isError) {
     return <div className="m-5">Failed to fetch data</div>;
@@ -12,7 +16,35 @@ const CardSalesSummary = () => {
 
   return (
     <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl flex flex-col justify-between">
-      {isLoading ? <div className="m-5">Loading...</div> : <></>}
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          {/* Header */}
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Sales Summary
+            </h2>
+          </div>
+
+          {/* Body */}
+          <div>
+            {/* Body Header */}
+            <div className="flex justify-between items-center mb-6 px-7">
+              <div className="text-lg font-medium">
+                <p className="text-xs text-gray-400">Value</p>
+              </div>
+              <span className="text-2xl font-extrabold">
+                $
+                {(totalValueSum / 1000000).toLocaleString("en", {
+                  maximumFractionDigits: 2,
+                })}
+                m
+              </span>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
