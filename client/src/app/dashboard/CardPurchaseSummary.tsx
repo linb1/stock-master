f0682bf23ee6620b5a1c830e5cbbe80c129c4eec
Brian Lin <brian@differential.com>
2024-08-12 14:15:17 -0400

    initial card purchase summary

---
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// 2:48:00
+const CardPurchaseSummary = () => {
+  return (
+    <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500">
+      CardPurchaseSummary
+    </div>
+  );
+};
+
+export default CardPurchaseSummary;
