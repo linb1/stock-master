2b028f2af7c9e39bb83f5fe9873d36c50fc7a0e2
Brian Lin <brian@differential.com>
2024-08-12 17:30:41 -0400

    initial statcard

---
+++ client/src/app/dashboard/StatCard.tsx
@@ -0,0 +1,39 @@
+import { LucideIcon } from "lucide-react";
+import React from "react";
+
+type StatDetail = {
+  title: string;
+  amount: string;
+  changePercentage: number;
+  IconComponent: LucideIcon;
+};
+
+type StatCardProps = {
+  title: string;
+  primaryIcon: JSX.Element;
+  details: StatDetail[];
+  dateRange: string;
+};
+
+const StatCard = ({
+  title,
+  primaryIcon,
+  details,
+  dateRange,
+}: StatCardProps) => {
+  const formatPercentage = (value: number) => {
+    const signal = value >= 0 ? "+" : "";
+    return `${signal}${value.toFixed()}%`;
+  };
+
+  const getChangedColor = (value: number) => {
+    value >= 0 ? "text-green-500" : "text-red-500";
+  };
+  return (
+    <div className="md:row-span-1 xl:row-span-2 bg-white col-span-1 shawdow-md rounded-2xl flex flex-col justify-between">
+      StatCard
+    </div>
+  );
+};
+
+export default StatCard;
