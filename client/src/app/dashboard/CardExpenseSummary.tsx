a4a9df7c692ea7d0a02aaee115ad7414251a3042
Brian Lin <brian@differential.com>
2024-08-12 16:29:20 -0400

    add header to card expense summary

--- client/src/app/dashboard/CardExpenseSummary.tsx
+++ client/src/app/dashboard/CardExpenseSummary.tsx
@@ -1,9 +1,27 @@
 import { useGetDashboardMetricsQuery } from "@/state/api";
 import React from "react";
 
+const colors = ["#00C49F", "0088FE", "FFBB28"];
+
 const CardExpenseSummary = () => {
-  const { data, isLoading } = useGetDashboardMetricsQuery();
-  return <div className="row-span-3 bg-gray-500">CardExpenseSummary</div>;
+  const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+  return (
+    <div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          {/* Header */}
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Expense Summary
+            </h2>
+            <hr />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default CardExpenseSummary;
